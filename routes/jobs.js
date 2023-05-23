0c300614f2688a0a0d9b76f0b5d15e670bf005aa
GCastanheira <gustavocastanha@hotmail.com>
2023-05-23 14:47:59 -0300

    job-finder

---
+++ routes/jobs.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const Job = require('../models/Job')
+
+
+router.get('/test', (req, res) => {
+   res.render('index')
+})
+
+// detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+   where: {id: req.params.id}
+}).then(job => {
+   res.render('view', {
+      job
+   })
+}).catch(err => console.log(err))
+)
+
+router.get('/add', (req, res) => {
+   res.render('add')
+})
+
+// add jobs via post
+
+router.post('/add', (req, res) => {
+
+   let {title, salary, company, description, email, new_job} = req.body
+
+   // insert
+
+   Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+   })
+   .then(() => res.redirect('/'))
+   .catch(err => console.log(err))
+})
+
+module.exports = router
